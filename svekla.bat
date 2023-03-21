13c6197e5c7229d7001eb630b2407ba25a6b3722
mtshn <128024133+mtshn@users.noreply.github.com>
2023-03-21 15:41:31 +0300

    Add files via upload

---
+++ svekla.bat
@@ -0,0 +1,3 @@
+cd svekla
+.\jdk-16.0.2\bin\java  -cp svekla-0.0.2-jar-with-dependencies.jar  ru.ac.phyche.gcms.svekla.javafxgui.JavaFXGUIStarter
+pause
---
+++ svekla.sh
@@ -0,0 +1,3 @@
+cd svekla
+java -cp ./svekla-0.0.2-jar-with-dependencies.jar  ru.ac.phyche.gcms.svekla.javafxgui.JavaFXGUIStarter
+cd ..
